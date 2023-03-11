86507261da32dde53604ae6f18a06ce1c167a426
Kelium Jojoa Urrutia <keliumju@gmail.com>
2023-03-10 20:24:41 -0500

    Add bootstrap to vite app

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import './App.css'
 import Acordeon from './components/Acordeon'
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import AccordeonBootstrap from './components/AcordeonBootstrap';
+import { Button } from 'react-bootstrap';
+import { Accordion } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>Hello vite ;0</h1>
       <I18nextProvider i18n={i18n}>
+        <Button variant="primary">Primary</Button>{' '}
+        <Accordion></Accordion>
         <Acordeon />
+        <AccordeonBootstrap />
       </I18nextProvider>
     </div>
   )
